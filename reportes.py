b4c2f5b334bfb5a7794d9b367f8fcf4cb5a52988
SBLUV <bair.pasten@duocuc.cl>
2024-06-26 10:25:56 -0400

    Add files via upload

---
+++ reportes.py
@@ -0,0 +1,38 @@
+print("autores")
+
+print("gabriel garcia marquez")
+print("isabel allende")
+print("julio cortazar")
+print("jorge luis")
+print("pablo neruda")
+
+print("cantidad de libros prestados")
+
+print("1")
+print("0")
+print("10")
+print("7")
+print("5")
+
+import json
+def json (nombres:input):
+    def json (cantidad:int):
+
+     with open ("nombres"  ("cantidad", mode="r")) as read:
+        leerjson = json.load (read)
+        
+contador = 0
+
+for i in contador:
+    leercontador= ["nombres"]
+    leercontador=["cantidad"]
+
+    ["nombres= gabriel garcia marquez", "isabel allende", "julio cortazar","jorge luis","pablo neruda "]
+    ["cantidad= 1, 0, 10, 7, 5"]
+
+    for i in contador:
+       leercontador=["gabriel garcia marquez"][contador]["cantidad"]= 1
+       leercontador=["isabel allende"][contador]["cantidad"]= 0
+       leercontador=["julio cortazar"][contador]["cantidad"]= 10
+       leercontador=["jorge luis"][contador]["cantidad"]= 7
+       leercontador=["pablo nerudaa"][contador]["cantidad"]= 5
